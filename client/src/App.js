401e01ce92a4c1bdeac7fcebcb8907ce702e53c0
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-08-07 17:23:31 +0300

    add client and server parts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
 
-const App = () => (
-  <div className="App">
-    <h1>App</h1>
-  </div>
-);
+const App = () => {
+  const [bill, setBill] = useState({
+    name: "",
+    receiptId: 0,
+    price1: 0,
+    price2: 0
+  });
+  const [loading, setLoading] = useState(false);
+  const [downloading, setDownloading] = useState(false);
+
+  // const handleChange = (e) => {
+  //   e.persist();
+  //   setBill((prevBill) => ({
+  //     ...prevBill,
+  //     [e.target.name]: e.target.value
+  //   }));
+  // };
+
+  const handleChange = ({ target: { name, value } }) => {
+    setBill((prevBill) => ({
+      ...prevBill,
+      [name]: value
+    }));
+  };
+
+  const createAndDownloadPdf = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    axios
+      .post("/create-pdf", bill)
+      .then((res) => {
+        setLoading(false);
+        setDownloading(true);
+        axios
+          .get("/fetch-pdf", { responseType: "blob" })
+          .then((resp) => {
+            const pdfBlob = new Blob([resp.data], { type: "application/pdf" });
+            saveAs(pdfBlob, "bill.pdf");
+            setDownloading(false);
+          })
+          .catch((err) => {
+            setDownloading(false);
+            console.log(err.response);
+          });
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err.response.data.message);
+      });
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={createAndDownloadPdf}>
+        <input type="text" placeholder="Name" name="name" onChange={handleChange} />
+        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={handleChange} />
+        <input type="number" placeholder="Price 1" name="price1" onChange={handleChange} />
+        <input type="number" placeholder="Price 2" name="price2" onChange={handleChange} />
+        <button type="submit">Download PDF</button>
+      </form>
+      {loading && <div>Loading...</div>}
+      {downloading && <div>Downloading...</div>}
+    </div>
+  );
+};
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
 import express from "express";
 import logger from "morgan";
+import cors from "cors";
+import pdf from "html-pdf";
+
+import pdfTemplate from "./documents";
 
 const API_PORT = 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
+});
+
+app.post("/create-pdf", (req, res) => {
+  const options = {};
+  pdf.create(pdfTemplate(req.body), options).toFile("./result.pdf", (err) => {
+    if (err) {
+      res.status(503).json({ message: "Sorry, Service Unavailable!" });
+      return;
+    }
+    res.json(null); // success
+  });
+});
+
+app.get("/fetch-pdf", (req, res) => {
+  res.sendFile(`${__dirname}/result.pdf`);
 });
 
 app.listen(API_PORT, () => {
